2a919115a5485b9d4d1dcac0b8ffc70af85a8581
Your Name <anaeljonas@outlook.com>
2024-12-11 16:04:31 -0300

    feature: useNavigate

--- dev-keep-frontend/src/app/pages/notes/Notes.tsx
+++ dev-keep-frontend/src/app/pages/notes/Notes.tsx
@@ -1,12 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Notes = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/lp");
+  };
+
   return (
     <div>
       <h1>Notes</h1>
+
       <Link to="/lp">
-        <button>Ir para Landing Page</button>
+        <button>Ir para Landing Page (via Link)</button>
       </Link>
+
+      <br />
+
+      <button onClick={handleClick}>
+        Ir para Landing Page (via useNavigate)
+      </button>
     </div>
   );
 };
